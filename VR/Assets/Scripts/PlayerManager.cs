dc3c3f744bc821dac97dd6028c43722330de2e1d
ShadowKnight20 <48155299+ShadowKnight20@users.noreply.github.com>
2020-02-11 16:11:00 +0000

    comitting to pull.

---
+++ VR/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private int PlayerHealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerHealth <= 0)
+        {
+            //Gameover menu screen ect.
+        }
+        else if (PlayerHealth > 0)
+        {
+
+        }
+    }
+    public void DamageProcessor(int damage)
+        {
+        damage = 10;
+        //PlayerHealth=- damage();
+        //damage from stuff here espcialy enemies. 
+        }
+}
