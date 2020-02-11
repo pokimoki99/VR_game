073316dc6031d90958446963136d82586e104dde
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2020-02-11 17:48:20 +0000

    shield workin

--- VR_02/Assets/My Assets/Scripts/SimpleAttach.cs
+++ VR_02/Assets/My Assets/Scripts/SimpleAttach.cs
@@ -7,6 +7,17 @@ using Valve.VR.InteractionSystem;
 public class SimpleAttach : MonoBehaviour
 {
     private Interactable interactable;
+
+    protected bool attached = false;
+
+
+    [EnumFlags]
+    [Tooltip("The flags used to attach this object to the hand.")]
+    public Hand.AttachmentFlags attachmentFlags = Hand.AttachmentFlags.ParentToHand | Hand.AttachmentFlags.DetachFromOtherHand | Hand.AttachmentFlags.TurnOnKinematic;
+
+    [Tooltip("The local point which acts as a positional and rotational offset to use while held")]
+    public Transform attachmentOffset;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,20 +34,28 @@ public class SimpleAttach : MonoBehaviour
     }
     private void HandHoverUpdate(Hand hand)
     {
-        GrabTypes grabType = hand.GetGrabStarting();
-        bool isGrabEnding = hand.IsGrabEnding(gameObject);
-        
-        if ((interactable.attachedToHand = null) && (grabType != GrabTypes.None))
+        //GrabTypes grabType = hand.GetGrabStarting();
+        //bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        //if ((interactable.attachedToHand = null) && (grabType != GrabTypes.None))
+        //{
+        //    hand.AttachObject(gameObject, grabType);
+        //    hand.HoverLock(interactable);
+        //    hand.HideGrabHint();
+        //}
+
+        //else if (isGrabEnding)
+        //{
+        //    hand.DetachObject(gameObject);
+        //    hand.HoverUnlock(interactable);
+        //}
+
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+
+        if (startingGrabType != GrabTypes.None)
         {
-            hand.AttachObject(gameObject, grabType);
-            hand.HoverLock(interactable);
+            hand.AttachObject(gameObject, startingGrabType, attachmentFlags, attachmentOffset);
             hand.HideGrabHint();
         }
-        else if (isGrabEnding)
-        {
-            hand.DetachObject(gameObject);
-            hand.HoverUnlock(interactable);
-        }
     }
-
 }
