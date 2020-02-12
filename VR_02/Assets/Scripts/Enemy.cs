14df6450ca7f8dab330521c96cda8aedac941637
ShadowKnight20 <48155299+ShadowKnight20@users.noreply.github.com>
2020-02-12 14:29:40 +0000

    Look distance + follow the player?
    It could work if tested through VR?

---
+++ VR_02/Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Enemy : Interactable { 
+
+    //public override void Interact()
+    //{
+    //    base.Interact();
+    //}
+}
--- VR_02/Assets/Scripts/PlayerManager.cs
+++ VR_02/Assets/Scripts/PlayerManager.cs
@@ -4,29 +4,16 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    private int PlayerHealth = 100;
-    // Start is called before the first frame update
-    void Start()
+    #region Singleton
+    public static PlayerManager instance;
+
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (PlayerHealth <= 0)
-        {
-            //Gameover menu screen ect.
-        }
-        else if (PlayerHealth > 0)
-        {
+    #endregion
 
-        }
-    }
-    public void DamageProcessor(int damage)
-        {
-        damage = 10;
-        //PlayerHealth=- damage();
-        //damage from stuff here espcialy enemies. 
-        }
+    public GameObject player;
+
 }
