b928ac08177c3b244d903eb124d44b3d200c9d0a
ShadowKnight20 <48155299+ShadowKnight20@users.noreply.github.com>
2020-02-12 13:22:18 +0000

    Healthbar for Enemies and damage from sword
    (A good texture image for healthbar could be used)

---
+++ VR_02/Assets/Scripts/Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+
+    private int currentHealth;
+
+    public event Action<float> OnHealthPctChanged = delegate { };
+
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+    }
+    public void ModifyHealth (int amount)
+    {
+        currentHealth += amount;
+        float currentHealthPct = (float)currentHealth / (float)maxHealth;
+        OnHealthPctChanged(currentHealthPct);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Sword")
+        {
+            ModifyHealth(-10);
+        }
+        //if(other.gameObject.tag == "Bullet")
+        //{
+
+        //}
+    }
+}
