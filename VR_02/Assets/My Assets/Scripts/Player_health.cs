be3fd319da9a4dc95bf8b9845d1d2ea233726974
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2020-02-23 15:19:35 +0000

    health bar and damageable obstacles

---
+++ VR_02/Assets/My Assets/Scripts/Player_health.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Player_health : MonoBehaviour
+{
+
+    private Material material;
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        GetComponent<Health>().OnHealthPctChanged += HandleHealthChanged;
+    }
+
+    private void HandleHealthChanged(float pct)
+    {
+        //StartCoroutine(ChangeToPct(pct));
+        material.SetFloat("Cutoff", 0f + pct);
+    }
+}
