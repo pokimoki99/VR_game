fbeda91cdf2d34e72b6e969865954ba1fad3e291
JohnnyJuliano <48156207+JohnnyJuliano@users.noreply.github.com>
2020-03-02 12:37:34 +0000

    done
    done

--- VR_02/Assets/My Assets/Scripts/EnemySpawnerScript.cs
+++ VR_02/Assets/My Assets/Scripts/EnemySpawnerScript.cs
@@ -8,19 +8,26 @@ public class EnemySpawnerScript : MonoBehaviour
     public int xPos;
     public int zPos;
     public int enemyCount;
+
+    private static int time;
+
     void Start()
     {
         StartCoroutine(EnemyDrop());
     }
     IEnumerator EnemyDrop()
     {
-        while (enemyCount < 10)
+        time = timer.timerstart;
+        if (time <= 200)
         {
-            xPos = Random.Range(0, 26);
-            zPos = Random.Range(11, 30);
-            Instantiate(theEnemy, new Vector3(xPos, 29, zPos), Quaternion.identity);
-            yield return new WaitForSeconds(1.0f);
-            enemyCount += 1;
+            while (enemyCount < 10)
+            {
+                xPos = Random.Range(0, 26);
+                zPos = Random.Range(11, 30);
+                Instantiate(theEnemy, new Vector3(xPos, 29, zPos), Quaternion.identity);
+                yield return new WaitForSeconds(1.0f);
+                enemyCount += 1;
+            }
         }
     }
 }
